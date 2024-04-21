70599585f8f319379774472a9a1a4401b778e103
Cyrille 0.2 <cyrille.morales@fgciph.com>
2024-04-21 19:02:09 +0800

    Add grades model

---
+++ PanelingSystem/Models/GradeModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PanelingSystem.Models
+{
+    [Table("Grades")]
+    [PrimaryKey("GradeId")]
+    public class GradeModel
+    {
+        public int GradeId { get; set; }
+        public int UserId { get; set; }
+        public double Grade { get; set; }
+        [ForeignKey("UserId")]
+        public UserAccountModel Student {  get; set; }
+
+    }
+}
